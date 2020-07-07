1812022bcd33ffa2317b77dd4a789751e8003408
adisakshya <adisakshya@triveous.com>
2020-07-07 20:39:03 +0530

    added create-entity-response common-dto

--- src/common/dto/index.ts
+++ src/common/dto/index.ts
@@ -2,3 +2,4 @@ export * from "./paging-query.dto"
 export * from "./paginated-response.dto"
 export * from "./query-by-ids-params.dto"
 export * from "./base-header.dto"
+export * from "./create-entity-response.dto";
