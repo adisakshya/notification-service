dbc044d7ccbc87c6f38e353e9016921dc730018e
adisakshya <adisakshya@triveous.com>
2020-07-07 15:08:09 +0530

    added swagger-ui

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function configure() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Notification Service')
+    .setDescription('Internal API documentation for Notification Service')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 
--- src/src/ping/ping.controller.ts
+++ src/src/ping/ping.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PingService } from './ping.service';
+import {ApiOperation, ApiTags} from "@nestjs/swagger";
 
+@ApiTags("Ping")
 @Controller()
 export class PingController {
     constructor(private readonly pingService: PingService) {}
