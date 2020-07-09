d08d2223ccd9d47f39f4fe1f18974759efd8a95d
adisakshya <adisakshya@triveous.com>
2020-07-09 20:09:05 +0530

    added create event-pattern

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
 import { BoomExceptionFilter } from "@common/expection-filter";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import {FireBase} from '@common/firebase/firebase';
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
 
 async function configure() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      url: 'http://192.168.99.100:6379',
+    }
+  });
   app.useGlobalFilters(app.get(BoomExceptionFilter));
-  const options = new DocumentBuilder()
-    .setTitle('Notification Service')
-    .setDescription('Internal API documentation for Notification Service')
-    .setVersion('1.0.0')
-    .build();
   new FireBase().configure();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+  await app.listen(() => console.log('Notification Service is active...'));
 }
 
 configure();
--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,15 +1,26 @@
 import { InjectQueue } from '@nestjs/bull';
 import { Controller, Post } from '@nestjs/common';
 import { Queue } from 'bull';
+import { EventPattern, Payload, Ctx, RedisContext } from '@nestjs/microservices';
 
 @Controller('notification')
 export class NotificationController {
   constructor(@InjectQueue('notification') private readonly notificationQueue: Queue) {}
 
-  @Post('create')
-  async transcode() {
-    await this.notificationQueue.add('create', {
-      name: 'My Notification'
-    });
+  @EventPattern('create')
+  async create(@Payload() reminder: any, @Ctx() context: RedisContext) {
+    console.log(`Channel: ${context.getChannel()}`);
+    
+    // Set delay time
+    const delay = new Date(reminder.date).getTime() - new Date().getTime();
+    
+    // Set payload
+    const payload = {
+      id: reminder.id,
+      userId: reminder.userId
+    }
+    
+    // Schedule notification
+    await this.notificationQueue.add('create', payload, { delay: delay });
   }
 }
